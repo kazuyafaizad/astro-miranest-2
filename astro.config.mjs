327ff264d4ba57bc9499b2c4950f045e9f0fdf81
kazuyafaizad <kazuyafaizad@gmail.com>
2022-01-13 09:49:32 +0800

    first setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // Full Astro Configuration API Documentation:
 // https://docs.astro.build/reference/configuration-reference
-
+import { imagetools } from "vite-imagetools";
 // @type-check enabled!
 // VSCode and other TypeScript-enabled text editors will provide auto-completion,
 // helpful tooltips, and warnings if your exported object is invalid.
@@ -8,6 +8,10 @@
 
 // @ts-check
 export default /** @type {import('astro').AstroUserConfig} */ ({
-	// Enable the Preact renderer to support Preact JSX components.
-	renderers: ['@astrojs/renderer-preact'],
+  // Enable the Preact renderer to support Preact JSX components.
+  renderers: ["@astrojs/renderer-preact"],
+  vite: {
+    // for example only - adapt for your own project
+    plugins: [imagetools({ force: true })],
+  },
 });

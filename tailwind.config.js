77764e74840029b5ed712d9f6aba3f56d018f4e5
kazuyafaizad <kazuyafaizad@gmail.com>
2022-01-20 10:05:32 +0800

    Images

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        greenTheme: "#1F3939",
+        greenLightTheme: "#005656",
+        lightRed: "#98847d",
+        darkGray: "#333333",
+      },
+    },
+  },
 };

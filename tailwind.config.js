a96c1401f00ea2d83a31f0dece251362eaa7537a
kazuyafaizad <kazuyafaizad@gmail.com>
2022-01-21 23:24:52 +0800

    Top Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,22 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        greenTheme: "#1F3939",
-        greenLightTheme: "#005656",
-        lightRed: "#98847d",
-        darkGray: "#333333",
+        "main-green": "#1F3939",
+        "main-light-green": "#005656",
+        "main-red": "#98847d",
+        black: "#333333",
+        golden: "#BCA674",
+        goldeni: "#A07500",
+        "main-gray": "#F9F9F9",
+      },
+      fontSize: {
+        base: ["16px", "32px"],
+        md: ["18px", "50px"],
+        h2: ["20px", "40px"],
+        h3: ["18px", "32px"],
+        "2-1/2xl": ["30px", "35px"],
+        "3xl": ["24px", "48px"],
+        "4xl": ["35px", "70px"],
       },
     },
   },

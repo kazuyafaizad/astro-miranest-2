bc29ea5787296c18e925fbb0ed3a1b0f110d93a9
kazuyafaizad <kazuyafaizad@gmail.com>
2022-02-03 14:55:36 +0800

    fixed1

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,14 @@
 // VSCode and other TypeScript-enabled text editors will provide auto-completion,
 // helpful tooltips, and warnings if your exported object is invalid.
 // You can disable this by removing "@ts-check" and `@type` comments below.
-
 // @ts-check
 export default /** @type {import('astro').AstroUserConfig} */ ({
-	// Enable the Preact renderer to support Preact JSX components.
-	renderers: ['@astrojs/renderer-preact'],
+  // Enable the Preact renderer to support Preact JSX components.
+  renderers: ["@astrojs/renderer-preact"],
+  buildOptions: {
+    sitemap: true,
+  },
+  devOptions: {
+    hostname: "0.0.0.0",
+  },
 });
